3d2a8472ebfb9dd39aca69fe735df7e17f932e50
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-09-17 11:21:16 +0300

    set up local docker compose to run postgresql

--- bot/config.py
+++ bot/config.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, PostgresDsn, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -7,8 +7,28 @@ class BotSettings(BaseModel):
     token: str
 
 
+class DatabaseSettings(BaseModel):
+    username: str
+    password: str | None = None
+    host: str
+    port: int = Field(ge=1, le=65535)
+    name: str
+
+    @computed_field
+    def url(self) -> str:
+        return PostgresDsn.build(
+            scheme="postgresql+asyncpg",
+            username=self.username,
+            password=self.password,
+            host=self.host,
+            port=self.port,
+            path=self.name,
+        ).unicode_string()
+
+
 class Settings(BaseSettings):
     bot: BotSettings
+    db: DatabaseSettings
 
     model_config = SettingsConfigDict(env_nested_delimiter="__", env_ignore_empty=True)
 
--- justfile
+++ justfile
@@ -1,8 +1,16 @@
 default: fmt fix
 
 # Startup Commands
+compose_file := "docker-compose-local.yaml"
+
 run:
     python -m bot.main
+
+up:
+    docker compose -f {{compose_file}} up -d
+
+down:
+    docker compose -f {{compose_file}} down
 
 # Code Styling
 fmt:
