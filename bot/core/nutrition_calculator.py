58daa3a945c39a3936ab81addcd1a13ff37f92fd
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-09-18 17:07:41 +0300

    create functions to calculate daily water, fiber, salt and caffeine needs

--- bot/core/nutrition_calculator.py
+++ bot/core/nutrition_calculator.py
@@ -104,3 +104,78 @@ def calc_carbohydrates(*, gender: Gender, age: int, height: float, weight: float
     proteins = calc_proteins(weight=weight, fat_pct=fat_pct)
     fats = calc_fats(weight=weight, fat_pct=fat_pct)
     return (calories - (proteins * 4) + (fats * 9)) / 4
+
+
+def calc_water(*, weight: float, fat_pct: int) -> float:
+    """Calculate daily water needs.
+
+    Args:
+        weight: Weight of the person in kilograms.
+        fat_pct: Body fat percentage.
+
+    Returns:
+        The daily water needs in liters/day.
+
+    """
+    lbm = calc_lbm(full_weight=weight, fat_pct=fat_pct)
+    return lbm / 20
+
+
+def calc_fiber(*, gender: Gender, age: int, height: float, weight: float, fat_pct: int, amr: float) -> float:
+    """Calculate daily fiber needs.
+
+    Args:
+        gender: Gender of the person.
+        age: Age of the person in years.
+        height: Height of the person in centimeters.
+        weight: Weight of the person in kilograms.
+        fat_pct: Body fat percentage.
+        amr: Activity Multiplier Rate (e.g., 1.2 for sedentary, 1.375 for lightly active, etc.).
+
+    Returns:
+        The daily fiber needs in grams/day.
+
+    """
+    calories = calc_calories(gender=gender, age=age, height=height, weight=weight, fat_pct=fat_pct, amr=amr)
+    return calories / 1000 * 10
+
+
+def calc_salt(*, weight: float, fat_pct: int) -> float:
+    """Calculate daily salt needs.
+
+    Args:
+        weight: Weight of the person in kilograms.
+        fat_pct: Body fat percentage.
+
+    Returns:
+        The daily salt needs in grams/day.
+
+    """
+    lbm = calc_lbm(full_weight=weight, fat_pct=fat_pct)
+    return lbm / 10
+
+
+def calc_caffeine_norm(*, weight: float) -> float:
+    """Calculate normal daily caffeine intake.
+
+    Args:
+        weight: Weight of the person in kilograms.
+
+    Returns:
+        The normal daily caffeine intake in milligrams/day.
+
+    """
+    return weight * 2.5
+
+
+def calc_caffeine_max(*, weight: float) -> float:
+    """Calculate maximum daily caffeine intake.
+
+    Args:
+        weight: Weight of the person in kilograms.
+
+    Returns:
+        The maximum daily caffeine intake in milligrams/day.
+
+    """
+    return weight * 5
