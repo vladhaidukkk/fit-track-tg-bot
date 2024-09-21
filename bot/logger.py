6891cb961d8c6eaa7f11e2fa3fa6ec064aac4bfc
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-09-21 20:06:17 +0300

    configure logging with the help of dict

--- bot/logger.py
+++ bot/logger.py
@@ -1,19 +1,39 @@
 import logging
-import sys
+import logging.config
 from typing import Literal
-
-import colorlog
 
 LogLevelName = Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
 
 
+def get_logging_config(root_level_name: LogLevelName = "INFO") -> dict[str, any]:
+    return {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "colored": {
+                "()": "colorlog.ColoredFormatter",
+                "format": "%(log_color)s%(levelname)s%(reset)s:%(name)s:%(log_color)s%(message)s%(reset)s",
+            }
+        },
+        "handlers": {
+            "stdout": {
+                "class": "logging.StreamHandler",
+                "formatter": "colored",
+                "stream": "ext://sys.stdout",
+            },
+        },
+        "loggers": {
+            "root": {
+                "level": root_level_name,
+                "handlers": ["stdout"],
+            }
+        }
+    }
+
+
 def configure_logging(level_name: LogLevelName = "INFO") -> None:
-    stdout_handler = colorlog.StreamHandler(stream=sys.stdout)
-    colored_formatter = colorlog.ColoredFormatter(
-        "%(log_color)s%(levelname)s%(reset)s:%(name)s:%(log_color)s%(message)s%(reset)s"
-    )
-    stdout_handler.setFormatter(colored_formatter)
-    logging.basicConfig(level=level_name, handlers=[stdout_handler])
+    config = get_logging_config(root_level_name=level_name)
+    logging.config.dictConfig(config)
 
 
 logger = logging.getLogger("bot")
