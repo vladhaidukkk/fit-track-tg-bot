89ecda0cce56f667bbeecd9e28ce25fef65cc168
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-09-21 19:23:54 +0300

    add colored stdout handler to root logger

--- bot/logger.py
+++ bot/logger.py
@@ -1,11 +1,19 @@
 import logging
+import sys
 from typing import Literal
+
+import colorlog
 
 LogLevelName = Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
 
 
 def configure_logging(level_name: LogLevelName = "INFO") -> None:
-    logging.basicConfig(level=level_name)
+    stdout_handler = colorlog.StreamHandler(stream=sys.stdout)
+    colored_formatter = colorlog.ColoredFormatter(
+        "%(log_color)s%(levelname)s%(reset)s:%(name)s:%(log_color)s%(message)s%(reset)s"
+    )
+    stdout_handler.setFormatter(colored_formatter)
+    logging.basicConfig(level=level_name, handlers=[stdout_handler])
 
 
 logger = logging.getLogger("bot")
