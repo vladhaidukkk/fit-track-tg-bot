31a27eff97d44b23390a804d5d0bd5e8e6e34497
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-09-25 17:31:01 +0300

    comment out calc pfc button

--- bot/keyboards/root.py
+++ bot/keyboards/root.py
@@ -4,7 +4,7 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
 
 class RootKeyboardText(StrEnum):
-    CALC_CPFC = "🍏 Розрахувати КБЖВ"
+    CALC_CPFC = "🥗 Розрахувати калорійність"
     CALC_PFC = "🍎 Розрахувати БЖВ"
     LEAVE_FEEDBACK = "💬 Залишити побажання"
 
@@ -12,7 +12,10 @@ class RootKeyboardText(StrEnum):
 def root_keyboard() -> ReplyKeyboardMarkup:
     return ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text=RootKeyboardText.CALC_CPFC), KeyboardButton(text=RootKeyboardText.CALC_PFC)],
+            [
+                KeyboardButton(text=RootKeyboardText.CALC_CPFC),
+                # KeyboardButton(text=RootKeyboardText.CALC_PFC),
+            ],
             [KeyboardButton(text=RootKeyboardText.LEAVE_FEEDBACK)],
         ],
         resize_keyboard=True,
