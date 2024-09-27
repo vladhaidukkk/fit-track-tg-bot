ad0c702c5e995090abc596f77636285ef981b472
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-09-27 09:28:37 +0300

    create cancel command and do the same action on start

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,7 +1,12 @@
 from aiogram import Router
 
-from .commands import router as commands_router
+from .commands import primary_router as primary_commands_router
+from .commands import secondary_router as secondary_commands_router
 from .surveys import router as surveys_router
 
 router = Router(name=__name__)
-router.include_routers(surveys_router, commands_router)
+router.include_routers(
+    primary_commands_router,
+    surveys_router,
+    secondary_commands_router,
+)
--- bot/handlers/commands.py
+++ bot/handlers/commands.py
@@ -1,5 +1,6 @@
 from aiogram import Router
 from aiogram.filters import Command, CommandStart
+from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 from aiogram.utils import markdown as md
 
@@ -7,12 +8,16 @@ from bot.config import settings
 from bot.db.models import UserModel
 from bot.db.queries import add_user, update_user
 from bot.keyboards.root import root_keyboard
+from bot.utils.survey_utils import clear_messages
 
-router = Router(name=__name__)
+primary_router = Router(name=f"{__name__}:primary")
 
 
-@router.message(CommandStart())
-async def start_command_handler(message: Message, user: UserModel | None) -> None:
+@primary_router.message(CommandStart())
+async def start_command_handler(message: Message, state: FSMContext, user: UserModel | None) -> None:
+    await clear_messages(bot=message.bot, chat_id=message.chat.id, state=state)
+    await state.clear()
+
     if not user:
         await add_user(id_=message.from_user.id, username=message.from_user.username)
     if user and user.username != message.from_user.username:
@@ -28,6 +33,15 @@ async def start_command_handler(message: Message, user: UserModel | None) -> Non
     )
 
 
-@router.message(Command("id"))
+@primary_router.message(Command("cancel"))
+async def cancel_command_handler(message: Message, state: FSMContext) -> None:
+    await clear_messages(bot=message.bot, chat_id=message.chat.id, state=state)
+    await state.clear()
+
+
+secondary_router = Router(name=f"{__name__}:secondary")
+
+
+@secondary_router.message(Command("id"))
 async def id_command_handler(message: Message) -> None:
     await message.answer(md.hcode(message.from_user.id))
