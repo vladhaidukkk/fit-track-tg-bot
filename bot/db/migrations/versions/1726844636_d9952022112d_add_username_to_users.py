911db1137032538fcd5adc4319916538a3130419
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-10-16 15:49:34 +0300

    update migrations to work with sqlite

--- bot/db/migrations/versions/1726844636_d9952022112d_add_username_to_users.py
+++ bot/db/migrations/versions/1726844636_d9952022112d_add_username_to_users.py
@@ -18,10 +18,12 @@ depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
-    op.add_column("users", sa.Column("username", sa.String(), nullable=True))
-    op.create_unique_constraint(op.f("uq_users_username"), "users", ["username"])
+    with op.batch_alter_table("users") as batch_op:
+        batch_op.add_column(sa.Column("username", sa.String(), nullable=True))
+        batch_op.create_unique_constraint(op.f("uq_users_username"), ["username"])
 
 
 def downgrade() -> None:
-    op.drop_constraint(op.f("uq_users_username"), "users", type_="unique")
-    op.drop_column("users", "username")
+    with op.batch_alter_table("users") as batch_op:
+        batch_op.drop_constraint(op.f("uq_users_username"), type_="unique")
+        batch_op.drop_column("username")
